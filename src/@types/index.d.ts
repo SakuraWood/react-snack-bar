14048ea6c4d378a7cb1e25846bb3ba79da8e1044
Lee Sure <smartadelesure@gmail.com>
2020-05-26 12:13:34 +0800

    --

--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -1,22 +1,8 @@
 declare interface SnackBarProps {
-  fontSize?: number
-  align?: 'top' | 'bottom' | 'center'
+  fontSize?: number;
+  align?: 'top' | 'bottom' | 'center';
 }
 
-declare const Color = {
-  WARN: '#f09925',
-  SUCCESS: '#51ba4e',
-  ERROR: '#d32f2f',
-  INFO: '#fff',
-}
+declare type Type = 'WARN' | 'SUCCESS' | 'ERROR' | 'INFO';
 
-declare const FontColor = {
-  WARN: '#fff',
-  SUCCESS: '#fff',
-  ERROR: '#fff',
-  INFO: '#404657',
-}
-
-declare type Type = 'WARN' | 'SUCCESS' | 'ERROR' | 'INFO'
-
-declare function useSnackBar(force: boolean): void
+declare function useSnackBar(force: boolean): void;
--- src/index.tsx
+++ src/index.tsx
@@ -1,104 +1,81 @@
-import React, {
-  useState,
-  forwardRef,
-  useImperativeHandle,
-  useRef,
-  useEffect,
-} from 'react';
-import ReactDOM from 'react-dom';
-require('./assets/css/index.css');
+/* eslint-disable no-nested-ternary */
+import React, { useState, forwardRef, useImperativeHandle, useRef, useEffect } from 'react'
+import ReactDOM from 'react-dom'
+
+require('./assets/css/index.css')
 
 const Img = {
   WARN: require('./assets/img/warn.png'),
   SUCCESS: require('./assets/img/smile.png'),
   ERROR: require('./assets/img/error.png'),
-  INFO: require('./assets/img/info.png'),
-};
+  INFO: require('./assets/img/info.png')
+}
 
-let timer: any;
-let animTimer: any;
+let timer: any
 
 export interface SnackBarProps {
-  fontSize?: number;
-  align?: 'top' | 'bottom' | 'center';
-  rtl?: boolean;
+  fontSize?: number
+  align?: 'top' | 'bottom' | 'center'
+  rtl?: boolean
 }
 
 const Color = {
   WARN: '#f09925',
   SUCCESS: '#51ba4e',
   ERROR: '#d32f2f',
-  INFO: '#fff',
-};
+  INFO: '#fff'
+}
 
 const FontColor = {
   WARN: '#fff',
   SUCCESS: '#fff',
   ERROR: '#fff',
-  INFO: '#404657',
-};
+  INFO: '#404657'
+}
 
-export type Type = 'WARN' | 'SUCCESS' | 'ERROR' | 'INFO';
+export type Type = 'WARN' | 'SUCCESS' | 'ERROR' | 'INFO'
 
-const SnackBarContainer: React.FC<SnackBarProps> = (
-  props: SnackBarProps,
-  ref
-) => {
-  const { fontSize = 14, align = 'bottom', rtl = false } = props;
+const SnackBarContainer: React.FC<SnackBarProps> = (props: SnackBarProps, ref) => {
+  const { fontSize = 14, align = 'bottom', rtl = false } = props
 
-  const [show, setShow] = useState(false);
+  const [show, setShow] = useState(false)
 
-  const [hide, setHide] = useState(true);
+  const [backgroundColor, setBackgroundColor] = useState(Color.ERROR)
 
-  const [backgroundColor, setBackgroundColor] = useState(Color.ERROR);
+  const [color, setColor] = useState(FontColor.ERROR)
 
-  const [color, setColor] = useState(FontColor.ERROR);
+  const [img, setImg] = useState(Img.ERROR)
 
-  const [img, setImg] = useState(Img.ERROR);
+  const [text, setText] = useState('')
 
-  const [text, setText] = useState('');
+  const alignStyle = align === 'bottom' ? { bottom: -50 } : align === 'top' ? { top: -50 } : { top: '50%', transform: 'translateY(-50%)' }
 
-  const alignStyle =
-    align === 'bottom'
-      ? { bottom: -40 }
-      : align === 'top'
-      ? { top: -40 }
-      : { top: '50%', transform: 'translateY(-50%)' };
+  const time = align === 'center' ? '300ms' : '500ms'
 
-  const time = align === 'center' ? '300ms' : '500ms';
+  const transformStyle = { animation: `${align}snackbaranimation ${time} ease forwards` }
 
-  const transformStyle = {
-    animation: `${align}snackbaranimation ${time} ease forwards`,
-  };
-
-  const transformStyleNoShow = {
-    animation: `${align}snackbaranimationnoshow ${time} ease forwards`,
-  };
+  const transformStyleNoShow = { animation: `${align}snackbaranimationnoshow ${time} ease forwards` }
 
   useImperativeHandle(ref, () => ({
     show: (text: string, type: Type = 'ERROR') => {
-      setText(text);
-      setBackgroundColor(Color[type]);
-      setColor(FontColor[type]);
-      setImg(Img[type]);
-      showSnackBar();
-    },
-  }));
+      setText(text)
+      setBackgroundColor(Color[type])
+      setColor(FontColor[type])
+      setImg(Img[type])
+      showSnackBar()
+    }
+  }))
 
   const showSnackBar = () => {
-    setHide(false);
-    setShow(true);
-    clearTimeout(timer);
+    console.log('show snack bar')
+    setShow(true)
+    clearTimeout(timer)
     timer = setTimeout(() => {
-      setShow(false);
-      clearTimeout(animTimer);
-      animTimer = setTimeout(() => {
-        setHide(true);
-      }, 500);
-    }, 2000);
-  };
+      setShow(false)
+    }, 2000)
+  }
 
-  return hide ? null : (
+  return (
     <div
       style={{
         ...alignStyle,
@@ -106,58 +83,51 @@ const SnackBarContainer: React.FC<SnackBarProps> = (
         zIndex: 9999,
         padding: 10,
         display: 'flex',
-        flexDirection: rtl ? 'row' : 'row-reverse',
-        justifyContent: 'flex-end',
+        flexDirection: rtl ? 'row-reverse' : 'row',
+        justifyContent: 'flex-start',
         alignItems: 'center',
         width: '92%',
-        left: '4%',
-        position: 'fixed',
+        maxWidth: 400,
+        position: 'absolute',
         height: 40,
         backgroundColor,
         borderRadius: 4,
         boxSizing: 'border-box',
-        boxShadow:
-          '-6px 3px 5px -1px rgba(0,0,0,0.2), 0px 6px 10px 0px rgba(0,0,0,0.14), 0px 1px 18px 0px rgba(0,0,0,0.12)',
+        boxShadow: '-6px 3px 5px -1px rgba(0,0,0,0.2), 0px 6px 10px 0px rgba(0,0,0,0.14), 0px 1px 18px 0px rgba(0,0,0,0.12)'
       }}
     >
-      <div style={{ fontSize, color }}>{text}</div>
-      <img
-        src={img}
-        alt="img"
-        style={{ width: 20, height: 20, marginLeft: 16 }}
-      ></img>
+      <img src={img} alt="img" style={{ width: 20, height: 20 }} />
+      <div style={{ fontSize, color, marginLeft: 10, marginRight: 10 }}>{text}</div>
     </div>
-  );
-};
+  )
+}
 
-const SnackBar: any = forwardRef(SnackBarContainer as any);
+const SnackBar: any = forwardRef(SnackBarContainer as any)
 
-export default SnackBar;
+export default SnackBar
 
-export const useSnackBar = (
-  props: SnackBarProps = { fontSize: 14, align: 'bottom' },
-  force = false
-) => {
-  const { fontSize, align }: any = props;
-  const ref: any = useRef(null);
+export const useSnackBar = (props: SnackBarProps = { fontSize: 14, align: 'bottom', rtl: false }, force = false) => {
+  const { fontSize, align, rtl }: any = props
+  const ref: any = useRef(null)
   useEffect(() => {
-    const style = getComputedStyle(document.body);
-    console.warn(style.boxSizing);
+    const style = getComputedStyle(document.body)
+    console.warn(style.boxSizing)
     if (!force && style.boxSizing && style.boxSizing !== 'border-box') {
-      console.warn(
-        'react-snack-bar: box-sizing is not border-box, it may produce some problems'
-      );
+      console.warn('react-snack-bar: box-sizing is not border-box, it may produce some problems')
     }
-    const div = document.createElement('div');
-    document.body.appendChild(div);
-    ReactDOM.render(
-      <SnackBar ref={ref} align={align} fontSize={fontSize}></SnackBar>,
-      div
-    );
-    return () => {};
-  }, []);
-  return {
-    show: (text: string, type: Type = 'ERROR') =>
-      ref.current && ref.current.show(text, type),
-  };
-};
+    const div = document.createElement('div')
+    div.style.width = '100%'
+    div.style.height = '100%'
+    div.style.position = 'absolute'
+    div.style.zIndex = '10000'
+    div.style.top = '0'
+    div.style.pointerEvents = 'none'
+    div.style.display = 'flex'
+    div.style.justifyContent = 'center'
+    document.body.appendChild(div)
+    ReactDOM.render(<SnackBar ref={ref} align={align} fontSize={fontSize} rtl={rtl} />, div)
+    return () => {}
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+  return { show: (text: string, type: Type = 'ERROR') => ref.current && ref.current.show(text, type) }
+}
